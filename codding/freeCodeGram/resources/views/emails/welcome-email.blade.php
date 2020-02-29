d7b049b7c115b589f83d87fc018834caaa2b937c
zia <ziagulzia@gmail.com>
2020-02-29 09:42:09 +0500

    Adding file into repo

---
+++ codding/freeCodeGram/resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,11 @@
+@component('mail::message')
+# Welcome freeCodeGram
+
+This is a community of fellow developers and we love that you have joined us.
+
+
+All the best,<br>
+
+Zia Ur Rehman
+
+@endcomponent
